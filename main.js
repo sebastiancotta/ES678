180e80b72ae3c504a4bf904b6d856c93cb209371
sebastiancotta <sebastiancotta@gmail.com>
2020-05-24 15:01:11 -0300

    melhorando o aprendizado em javascript ES6

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert('hello world');
+// class TodoList {
+//     constructor() {
+//         this.todos = [];
+//     }
+
+//     addTodo() {
+//         this.todos.push('novo');
+//         console.log(this.todos);
+//     }
+// }
+
+// const MinhaLista = new TodoList();
+
+// document.getElementById('novotodo').onclick = function() {
+//     MinhaLista.addTodo();
+// }
+
+import { soma } from './funcoes'
+
+console.log(soma(1,2));
